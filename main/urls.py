8553120f059a26de860da9d122d727deec461d15
2pizzzza <amit.kurbanov.123.com@gmail.com>
2024-10-12 00:06:09 +0600

    added static

--- main/urls.py
+++ main/urls.py
@@ -41,7 +41,4 @@ urlpatterns = [
     path('', include('menu.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
-]
-
-if not settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
