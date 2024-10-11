babf4e4fbfb1177502c870b5770d7ab355442018
Kurbanov Amit <112865846+2pizzzza@users.noreply.github.com>
2024-10-12 01:27:42 +0600

    Update urls.py

--- menu/urls.py
+++ menu/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import CategoryListView, CategoryDetailView, MealsDetailView, CategoryWithMealsListView
+from .views import CategoryListView, CategoryDetailView, MealsDetailView, CategoryWithMealsListView, list_media_files
 
 urlpatterns = [
     # Category Endpoints
     path('categories/', CategoryListView.as_view(), name='category-list'),
     path('categories/<int:pk>/', CategoryDetailView.as_view(), name='category-detail'),
 
+    path('show/', list_media_files)
     # Get all categories with all his meals
     path('categories-with-meals/', CategoryWithMealsListView.as_view(), name='categories-with-meals'),
 
