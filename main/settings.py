5983be977a926af307aa7711c633c36b7845da9d
Kurbanov Amit <112865846+2pizzzza@users.noreply.github.com>
2024-10-12 00:56:17 +0600

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -139,11 +139,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
 
+MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / STATIC_URL / 'media/'
-
-MENU_IMAGE_FOLDER = 'menu/'
-
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
