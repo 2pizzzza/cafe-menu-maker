6bfdac548f9239e13d58dc57072adbdef60e19af
Kurbanov Amit <112865846+2pizzzza@users.noreply.github.com>
2024-10-12 01:18:00 +0600

    Update views.py

--- menu/views.py
+++ menu/views.py
@@ -18,3 +18,20 @@ class CategoryDetailView(generics.RetrieveAPIView):
 class MealsDetailView(generics.RetrieveAPIView):
     queryset = Meals.objects.all()
     serializer_class = MealsSerializer
+
+import os
+from django.conf import settings
+
+def list_media_files():
+    media_root = settings.MEDIA_ROOT
+    try:
+        files = os.listdir(media_root)
+        print("Files in directory:", media_root)
+        for file in files:
+            print(file)
+    except FileNotFoundError:
+        print(f"Directory not found: {media_root}")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+
+list_media_files()
