d42b7626b9e704aa992ebd8519c8404aee5621d9
Kurbanov Amit <112865846+2pizzzza@users.noreply.github.com>
2024-10-11 23:53:57 +0600

    Update urls.py

--- main/urls.py
+++ main/urls.py
@@ -43,5 +43,5 @@ urlpatterns = [
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
-if settings.DEBUG:
+if not settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
