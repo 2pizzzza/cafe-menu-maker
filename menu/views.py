f49dd80f915017b23825b7c04938056bd15c96fe
Kurbanov Amit <112865846+2pizzzza@users.noreply.github.com>
2024-10-12 01:29:50 +0600

    Update views.py

--- menu/views.py
+++ menu/views.py
@@ -20,18 +20,19 @@ class MealsDetailView(generics.RetrieveAPIView):
     serializer_class = MealsSerializer
 
 import os
+from django.http import JsonResponse
 from django.conf import settings
 
-def list_media_files():
+def list_files(request):
     media_root = settings.MEDIA_ROOT
+    files = []
     try:
-        files = os.listdir(media_root)
-        print("Files in directory:", media_root)
-        for file in files:
-            print(file)
+        for file in os.listdir(media_root):
+            if file != 'venv':
+                files.append(file)
     except FileNotFoundError:
-        print(f"Directory not found: {media_root}")
+        return JsonResponse({'error': 'Directory not found'}, status=404)
     except Exception as e:
-        print(f"An error occurred: {e}")
+        return JsonResponse({'error': str(e)}, status=500)
 
-list_media_files()
+    return JsonResponse({'files': files})
